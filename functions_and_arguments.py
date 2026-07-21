a59a1bf53180f797938831ca3fca12e8d1d9a92c
Oliver Jones <oljo1134@colorado.edu>
2026-07-21 10:41:52 -0600

    other python bs

---
+++ functions_and_arguments.py
@@ -0,0 +1,78 @@
+# we've been using functions from Day 1 (or almost): 
+# print(), type(), etc. 
+
+# a function is a MACHINE for doing something
+# takes an input then returns a result
+
+# print() <- takes any expression that we want to print
+# it prints stuff to the user
+
+# str() <- takes any expression and turns it into a string, and returns it to the user
+
+# what does it mean to return something?
+#mets take print(): 
+print("1234") # it is going to print "1234"
+my_content = print("1234")
+my_content # this is empty. print("1234") did not store anything in it
+# why? 
+
+# some functions (most) return something. think of them as a conveyor belt:
+# They are going to take an object on one side, do things to it, and then Return
+# the result to the other side
+
+# other functions are like an engine: you give them gas, and they are going to do something
+# but they won't hand you back anything
+
+# lets write functions together to better understand this distinction. 
+# we are going to write a function that takes a price, a rate, and returns the price updated with rate
+
+
+# we create a new function like this: 
+def print_total(price, rate): # def, followed by function name, and in parenthesis are arguments
+    # You will see that your curser moved to the right"
+    # this defines the body of the function. every code inside will define what fxn does
+    total = price * (1 + rate)
+    print(total)
+
+# we've created our functions, lets test drive it! 
+print_total(10, 0.1) # lets run this and practice tracing the code
+# lets say I want to store this result for later use: 
+my_total = print_total(10, 0.1)
+my_total # nothing inside my_total. This function is just an engine
+# lets solve this issue
+def calculate_total(price, rate): 
+    total = price * (1+ rate)
+    return total # on the other side of the conveyor belt, spit out the total
+my_total = calculate_total(10, 0.1)
+print(my_total) # success! this fxn calculated something, returned it back to me
+# and now I can store it into a variable
+# What happens if you don't store it? 
+calculate_total(10, 0.1) # it just prints it into the terminal
+# always better to have fxns RETURN stuff. gives more flexibility to user
+
+# more vocabulary: the inputs of a function are called arguments: 
+# two flavors: 
+# 1. Posiitonal Arguments: defined by the order you present them in
+round(3.14, 1) # Rounds the first number to the first number of digits in the second number
+round(1, 3.14) # TypeError. Float object cannot be interpretted as integer
+# the Position matters! 
+calculate_total(0.1, 10) # doesn't return 11.0, order matters
+# some fxns take a variable number of arguments 
+round(3.14) # it will give you 3. the second argument is not compulsory. it has a default which is 0
+print("abc")
+print("abc", "def", "ghi") # it will print all three strings. print() is a function that
+# takes an arbitrary number of arguments
+
+# 2. Second flavor of arguments: 'named' arguments or keyword arguments
+# these are arguments that are added by specifying their name:
+print('a', 'b', 'c', 'd', sep="*") # here sep is a named argument and I give it value *
+# named arguments are not compulsory, and have a default value. 
+# default argument for sep is a space
+print('A', 'B', 'C', 'D', sep='-', end='!')
+
+# One final but important thing: 
+def add_excitment(string): 
+    excited_string = string + " !!!!!"
+    return excited_string
+yo_mama = add_excitment("yo mama")
+print(yo_mama)
--- objects_and_methods.py
+++ objects_and_methods.py
@@ -26,3 +26,54 @@ this_is_a_string.upper() # a method requires () because they are actions
 # upper() translates everything to uppercase
 # all strings will have this method. All objects of a given type share the same methods
 this_is_a_string.lower() # everything is now lowercase
+
+#let me show you a few more methods for strings
+# strings can contain a lot of methods 
+# because there are a lot of things that we can do with them
+#we've already seen upper(), lower(), title()
+my_sentence = "hello, my name is oliver"
+my_sentence.title()
+# we've also seen endswith(), let me show you a few more
+lots_of_white_space = "        oliver "
+lots_of_white_space.strip()
+# let me show you a practical example of how these methods can be useful 
+entry = "   oljo1134@colorado.edu.   "
+# this is something someone could enter into a form
+is_it_edu = entry.endswith("edu") # false. because we didnt strip the space
+stripped_entry = entry.strip()
+is_it_edu_fr = stripped_entry.endswith("edu") # now it will be true
+# lets write it more cleanly
+
+is_it_edu_clean = entry.strip().endswith("edu")
+# This is called CHAINING. you can call methods on an object that is returned by another method
+
+# Common Errors with Methods and Properties
+entry.shout() # AttributeError: no attribute shout()
+# you cannot call a method that doesn't exist on the object
+price = 12
+price.numerator() # TypeError: int object is not callable
+# failing because an integer doesn't do anything. It is not a function or method.
+# IT was attempting to call a property. You can only call a method inside an obejct
+
+# a few more explorations
+price.is_integer # this is a method. Purple box and an action
+# we need the parenthesis to call the method! otherwise it won't do anything
+price.is_integer() # True
+
+# so far we've seen four big types of objects:
+# str, float, int, bool
+# In Python, you are often going to create other objects 
+# let me show you an object that is going to solve a problem we'bve had before
+
+from decimal import Decimal # not seen yet, soon! dw
+# what is Decimal? it is a factory to make new objects. Decimal objects
+# to create a str, you only need to put quotes
+# to create a Decimal object, we are going to use the decimal thing we just imported
+a = Decimal(".1")
+type(a)
+b = Decimal(" .2")
+type(b)
+print(.1 + .2) # what do we get? a FloatingPointError
+print(a + b) # if you print the sum of two decimal objects, you get an exact representation
+# Decimal solves this problem
+# if you reach into a Decimal object with the dot, you are going to see a lot of new methods and properties
